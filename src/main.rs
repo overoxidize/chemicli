07937e68bd6cb34322aff04b50947b91c55ed289
overoxidize <overoxidize@protonmail.com>
2023-07-27 11:48:09 -0400

    adding commands for metals

--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,67 @@ use chemicli::commands::element::*;
 use chemicli::commands::metalloids::Metalloids;
 use chemicli::commands::metals::Metals;
 use chemicli::commands::non_metals::NonMetals;
-use clap::{Arg, Args, Parser, Subcommand};
+use clap::{arg, Command, Arg};
 use std::collections::HashMap;
-#[derive(Debug, Parser)]
-#[clap(
-    name = "chemicli",
-    author,
-    version,
-    about = "A CLI for learning about chemistry"
-)]
-pub struct Chemicli {
-    #[clap(subcommand)]
-    pub command: Commands,
+
+
+fn cli() -> Command {
+    Command::new("chem")
+        .about("A CLI app for querying information about the periodic table.")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("ele")
+                    .arg(arg!(<ELEMENT> "Element to query about"))
+                    .subcommand(Command::new("prop").args_conflicts_with_subcommands(true)
+                    .short_flag('p')
+                    .arg(arg!(atmn: <ATOMIC_NUMBER> "atomic number").required(false))
+                    .arg(arg!(atw: <ATOMIC_WEIGHT> "atomic weight" ).required(false))
+                    .arg(arg!(wght: <WEIGHT> "weight of element").required(false))))
+        .subcommand( 
+                Command::new("mtl")
+                        .arg(arg!(sg: <SUBGROUP> "Metal subgroup"))
+                        .subcommand(Command::new("lanthanides").args_conflicts_with_subcommands(true)
+                        .short_flag('l')))
+                    //     .subcommand(
+                    // Command::new("sg")
+                    //         .arg(arg!(lnth: <LANTHANIDES> "Lanthanides")))).args_conflicts_with_subcommands(true)
+        
 }
 
-#[derive(Subcommand, Debug)]
-pub enum Commands {
-    /// Provides information about a specific element
-    #[clap(alias = "eln")]
-    ElementName { name: String },
-    #[clap(alias = "ele")]
-    Element(ElementCommands),
-    #[clap(alias = "met")]
-    /// Provides a list of elements categorized as metals
-    Metals(Metals),
-    #[clap(alias = "nmet")]
-    /// Provides a list of elements categorized as non-metals
-    NonMetals(NonMetals),
-    /// Provides a list of elements categorized as metalloids
-    #[clap(alias = "mtlo")]
-    Metalloids(Metalloids),
-}
+fn main () {
 
-fn main() {
-    let symbol_map: HashMap<String, ElementInfo> = HashMap::new();
-    struct ElementInfo;
-    let chemicli = Chemicli::parse();
+    let matches = cli().get_matches();
 
-    match chemicli.command {
-        Commands::Element(element_commands) => {
-            let query_type: Vec<String> = std::env::args().collect();
-            let mut arg_vec: Vec<String> = Vec::new();
-            println!("First arg is {:?}", query_type[1]);
-            arg_vec.push(query_type[1].clone());
-            let ElementCommands { element_args } = element_commands;
-            match element_args {
-                ElementArgs::AtmNum { atmn } => {
-                    println!("{:?}", atmn);
-                }
-                ElementArgs::Symbol { sym } => {
-                    if symbol_map.contains_key(&sym) {
-                        todo!()
-                    }
-                    println!("{:?}", sym);
-                }
-            }
+    let symbol_map: HashMap<String, String> = HashMap::new();
+    let arg_list: Vec<_> = std::env::args().collect();
+
+    let sym = &arg_list[2];
+
+    println!("sym is {:?}", sym);
+    match matches.subcommand() {
+        Some(("ele", sub_matches)) => {
+                // let sym = sub_matches.get_raw("H").unwrap().to_owned();
+            let sub_m = sub_matches.subcommand();
+
+            // println!("args {:?}", sub_m);
+            // match sub_m {
+            //     ("H", sub) => {
+            //         println!("Got it")
+            //     }
+            //     (_,_) => ()
+            // }
+            // match sub_m {
+            //     ("atmn", sub) => {
+
+            //     }
+            //     ("atw", sub) => {
+
+            //     }
+            //     (_,_) => todo!()
+            // }
         }
-        Commands::ElementName { name } => {
-            todo!()
-        }
-        Commands::Metals(_) => {
-            todo!()
-        }
-        Commands::NonMetals(_) => {
-            todo!()
-        }
-        Commands::Metalloids(_) => {
-            todo!()
-        }
+        None => {},
+        Some((_, _)) => {}
     }
+
 }
