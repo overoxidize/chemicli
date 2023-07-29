f3f15ebeffcd58801f79e1fffeb29c08749063e6
overoxidize <overoxidize@protonmail.com>
2023-07-29 12:54:05 -0400

    Building out a hashmap based structure for returning element correlated property queries

--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,84 @@ use chemicli::commands::metalloids::Metalloids;
 use chemicli::commands::metals::Metals;
 use chemicli::commands::non_metals::NonMetals;
 use clap::{arg, Command, Arg};
-use std::collections::HashMap;
-
-
+use std::{collections::HashMap, hash::Hash};
+// 
 fn cli() -> Command {
     Command::new("chem")
         .about("A CLI app for querying information about the periodic table.")
-        .subcommand_required(true)
         .subcommand(
             Command::new("ele")
                     .arg(arg!(<ELEMENT> "Element to query about"))
-                    .subcommand(Command::new("prop").args_conflicts_with_subcommands(true)
+                    .subcommand(Command::new("prop")
                     .short_flag('p')
                     .arg(arg!(atmn: <ATOMIC_NUMBER> "atomic number").required(false))
-                    .arg(arg!(atw: <ATOMIC_WEIGHT> "atomic weight" ).required(false))
+                    .arg(arg!(elvl: <ENERGY_LEVELS> "energy levels" ).required(false))
                     .arg(arg!(wght: <WEIGHT> "weight of element").required(false))))
         .subcommand( 
                 Command::new("mtl")
-                        .arg(arg!(sg: <SUBGROUP> "Metal subgroup"))
-                        .subcommand(Command::new("lanthanides").args_conflicts_with_subcommands(true)
-                        .short_flag('l')))
+                        .arg(arg!(sg: <SUBGROUP> "Metal subgroup").required(false))
+                        .arg(arg!(num: <NUMBER> "Number of metals").required(false))
+                        .subcommand(Command::new("lanthanides")))
                     //     .subcommand(
                     // Command::new("sg")
                     //         .arg(arg!(lnth: <LANTHANIDES> "Lanthanides")))).args_conflicts_with_subcommands(true)
         
 }
 
+
+type Element = HashMap<ElementName, ElementValue>;
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+struct EnergyLevels(u8);
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+struct ElementValue {
+    atomic_number: u16,
+    energy_levels: EnergyLevels,
+    element_name: ElementName,
+    
+}
+
+
+#[derive(PartialEq, Eq, Hash, Clone, Debug)]
+
+struct ElementName(String);
 fn main () {
+    let mut element_map = Element::new();
+    let mut symbol_map: HashMap<String, Element> = HashMap::new();
+    let hy = ElementName("Hydrogen".to_string());
+    let hy_e_levels = EnergyLevels(1);
+    
+    let hydrogen = ElementValue {
+        atomic_number: 1,
+        energy_levels: hy_e_levels,
+        element_name: hy.clone()
+    };
+    symbol_map.insert("H".to_string(), element_map.clone());
+    element_map.insert(hy, hydrogen);
+
+
+    let arg_list: Vec<_> = std::env::args().skip(2).collect();
 
     let matches = cli().get_matches();
 
-    let symbol_map: HashMap<String, String> = HashMap::new();
-    let arg_list: Vec<_> = std::env::args().collect();
+    let sym = &arg_list[0];
 
-    let sym = &arg_list[2];
+    let e_prop_one = &arg_list[2];
 
     println!("sym is {:?}", sym);
+    
     match matches.subcommand() {
         Some(("ele", sub_matches)) => {
-                // let sym = sub_matches.get_raw("H").unwrap().to_owned();
-            let sub_m = sub_matches.subcommand();
+            let ele_sym = Some(sym).unwrap();
+      
+            if symbol_map.contains_key(ele_sym) {
+                let queried_element = symbol_map.get(sym).expect("Symbol not found in map.");
+                let property_query_result = queried_element.get(&ElementName(e_prop_one.to_string())).expect("Element property not found.");
+                
+                println!("queried element is: {:?}", queried_element);
+                println!("first requested element property is: {:?}", e_prop_one);
+                println!("Queried property {:?} is {:?}", e_prop_one, property_query_result);
+            }
 
-            // println!("args {:?}", sub_m);
-            // match sub_m {
-            //     ("H", sub) => {
-            //         println!("Got it")
-            //     }
-            //     (_,_) => ()
-            // }
-            // match sub_m {
-            //     ("atmn", sub) => {
-
-            //     }
-            //     ("atw", sub) => {
-
-            //     }
-            //     (_,_) => todo!()
-            // }
         }
         None => {},
         Some((_, _)) => {}
