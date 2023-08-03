b417223fdcc3ee4a2e3c0d59212e6831cef8bb36
overoxidize <overoxidize@protonmail.com>
2023-08-03 17:25:06 -0400

    Removing commented out code.

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,6 @@ use std::collections::HashMap;
 /// Type alias for a HashMap associating elements with their known properties and subsequent values.
 
 type SymbolMap = HashMap<String, Option<ElementInfo>>;
-/// Newtype wrapping the atomic symbol of a given element.
-
 struct ElementInfo {
     atmn: Option<u8>
 }
@@ -33,20 +31,6 @@ pub enum Commands {
     /// the properties queried.
     #[clap(alias = "ele")]
     Element {ele: String, props: Vec<Option<String>>}
-
-    // #[clap(alias = "eln")]
-    // ElementName { name: String },
-    // #[clap(alias = "ele")]
-    // Element(ElementCommands),
-    // #[clap(alias = "met")]
-    // /// Provides a list of elements categorized as metals
-    // Metals(Metals),
-    // #[clap(alias = "nmet")]
-    // /// Provides a list of elements categorized as non-metals
-    // NonMetals(NonMetals),
-    // /// Provides a list of elements categorized as metalloids
-    // #[clap(alias = "mtlo")]
-    // Metalloids(Metalloids),
 }
 
 fn symbol_mapper(query_element: String, properties: Vec<String>) -> SymbolMap {
@@ -129,36 +113,5 @@ fn main() {
         None => ()
     }
     dbg!(&chemicli);
-    // match chemicli.command {
-    //     Commands::Element(element_commands) => {
-    //         let query_type: Vec<String> = std::env::args().collect();
-    //         let mut arg_vec: Vec<String> = Vec::new();
-    //         println!("First arg is {:?}", query_type[1]);
-    //         arg_vec.push(query_type[1].clone());
-    //         let ElementCommands { element_args } = element_commands;
-    //         match element_args {
-    //             ElementArgs::AtmNum { atmn } => {
-    //                 println!("{:?}", atmn);
-    //             }
-    //             ElementArgs::Symbol { sym } => {
-    //                 if symbol_map.contains_key(&sym) {
-    //                     todo!()
-    //                 }
-    //                 println!("{:?}", sym);
-    //             }
-    //         }
-    //     }
-    //     Commands::ElementName { name } => {
-    //         todo!()
-    //     }
-    //     Commands::Metals(_) => {
-    //         todo!()
-    //     }
-    //     Commands::NonMetals(_) => {
-    //         todo!()
-    //     }
-    //     Commands::Metalloids(_) => {
-    //         todo!()
-    //     }
-    // }
+ 
 }
